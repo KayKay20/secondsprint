dda4ad76eaa46fc071dbb6305128115c9b538328
KayKay20 <52742648+KayKay20@users.noreply.github.com>
2020-06-19 09:28:20 +0200

    Add files via upload

--- js/myJavaScript.js
+++ js/myJavaScript.js
@@ -1,9 +1,9 @@
 //function that makes the background color of the navbar changed when clicked
 function changeColorOfNavBar(){
 	//a tag changing the of the color
-	$('a').css('background-color', 'inherit')
+	$('a').css('background-color', 'inherit');
 	//if the navbar is clicked on the background color should turn red
-	$(event.target).css("background-color", "red")
+	$(event.target).css("background-color", "red");
 }
 //executing the function
 $('a').on('click', changeColorOfNavBar);
@@ -64,3 +64,49 @@ document.form1.onsubmit = function(e){
 		return false;
 	}
 }
+
+//slider
+let images = [
+    'pics/1.jpg',
+    'pics/2.jpg',
+    'pics/3.jpg',
+    'pics/4.jpg',
+    'pics/5.jpg',
+    'pics/6.jpg',
+    'pics/7.jpg',
+    'pics/8.jpg',
+    'pics/9.jpg',
+    'pics/10.jpg',
+    'pics/11.jpg',
+    'pics/12.jpg',
+    'pics/13.jpg',
+    'pics/14.jpg',
+    'pics/15.jpg',
+    'pics/16.jpg',
+    'pics/17.jpg'
+	];
+
+let i = 0;
+   function changeImage(){
+        if (i < images.length) {
+            document.getElementById("slider").src = images[i] 
+            i += 1;
+        }
+        else if(i >= images.length) {
+            i = 1;
+        }
+    }
+    setInterval(function(){ changeImage(); }, 3000);
+
+
+function big( value){
+	 value.style.width = "800px";
+	value.style.height = "400px";
+
+}
+
+function small(value){
+	value.style.width = "400px";
+	value.style.height = "200px";
+}
+
